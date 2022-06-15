ad1ea5344cc93a0f3d0c34fe440124935b8f27c0
AkinKazar <akazar@hepsiemlak.com>
2022-06-15 12:46:30 +0300

    first commit

---
+++ src/SumOfMultiplesOf4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SumOfMultiplesOf4 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number, sum = 0;
+
+        do {
+            System.out.print("Numara giriniz: ");
+            number = input.nextInt();
+
+            if (number % 4 == 0) {
+                sum += number;
+            }
+        } while (number % 2 == 0);
+
+        System.out.println("4 ve katlarının toplamı: " + sum);
+    }
+}
