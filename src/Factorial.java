9078bfd8f080f8c0d515e00adf1f85a366b28779
AkinKazar <akazar@hepsiemlak.com>
2022-06-16 14:34:44 +0300

    Combination And Factorial Class Added.

---
+++ src/Factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int number, sum = 1;
+
+        System.out.print("Bir sayı Giriniz: ");
+        number = input.nextInt();
+
+        for (int i = 1; i <= number; i++){
+            sum *= i;
+        }
+        System.out.println(sum);
+
+
+    }
+}
