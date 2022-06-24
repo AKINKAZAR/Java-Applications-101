02215c8b7a231687c51ff4a47de01dd365a85427
AkinKazar <akazar@hepsiemlak.com>
2022-06-24 09:55:17 +0300

    Added idea

--- src/DiamondWithStars.java
+++ src/DiamondWithStars.java
@@ -1,30 +1,31 @@
-
+import java.util.Scanner;
 
 public class DiamondWithStars {
     public static void main(String[] args) {
 
-        int n = 9;
 
-        for (int i = 1; i <= n; i++) {
+        Scanner input = new Scanner(System.in);
 
-            if (i < 6) {
-                for (int k = 1; k <= (n - i); k++) {
-                    System.out.print(" ");
-                }
-                for (int j = 1; j <= (2 * i) - 1; j++) {
-                    System.out.print("*");
-                }
-                System.out.println();
-            } else {
-                for (int t = 1; t <= n + i - 10; t++) {
-                    System.out.print(" ");
-                }
-                for (int y = 1; y <= 2*(n-i)+1; y++){
-                    System.out.print("*");
-                }
-                System.out.println();
+        System.out.print("Please enter a number: ");
+        int n = input.nextInt();
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
             }
-
+            for (int k = 1; k <= (2 * i) + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int a = n; a >= 1; a--) {
+            for (int b = 0; b <= (n - a); b++) {
+                System.out.print(" ");
+            }
+            for (int c = 1; c <= 2 * a - 1; c++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
         }
     }
 }
