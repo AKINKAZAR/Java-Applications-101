cdf0fb34f4733e93e8eb9682f76adad7319d24ef
AkinKazar <akazar@hepsiemlak.com>
2022-07-07 14:33:51 +0300

    PrimaryNumber added

---
+++ src/PrimeNumber.java
@@ -0,0 +1,18 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        boolean isPerfectNumber;
+
+        for (int i = 2; i <= 100; i++) {
+            isPerfectNumber = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPerfectNumber = false;
+                    break;
+                }
+            }
+            if (isPerfectNumber) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
