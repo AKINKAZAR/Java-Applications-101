930bd34d60b9287f7768fdd2d67442293e6512f7
AkinKazar <akazar@hepsiemlak.com>
2022-07-25 09:43:28 +0300

    RecursiveExponent class added

--- src/Functions/RecursiveExponent.java
+++ src/Functions/RecursiveExponent.java
@@ -1,5 +1,7 @@
 package Functions;
 
+import java.util.Scanner;
+
 public class RecursiveExponent {
 
     public static int power(int baseNumber, int exponentNumber) {
@@ -7,11 +9,18 @@ public class RecursiveExponent {
         if (exponentNumber == 1) {
             return baseNumber;
         }
-        return power(baseNumber,exponentNumber-1) * baseNumber;
+        return power(baseNumber, exponentNumber - 1) * baseNumber;
     }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-        System.out.println(power(5, 3));
+        int baseNumber, exponentNumber;
+        System.out.print("Taban değeri giriniz: ");
+        baseNumber = input.nextInt();
+        System.out.print("Üs değerini giriniz: ");
+        exponentNumber = input.nextInt();
+
+        System.out.println("Sonuç: " + power(baseNumber, exponentNumber));
     }
 }
