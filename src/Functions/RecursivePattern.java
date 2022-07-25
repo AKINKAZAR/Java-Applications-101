cb80c6c3abde37ab5a0c4c334190e99b9daccd0b
AkinKazar <akazar@hepsiemlak.com>
2022-07-25 15:59:08 +0300

    RecursivePattern class added

--- src/Functions/RecursivePattern.java
+++ src/Functions/RecursivePattern.java
@@ -1,2 +1,33 @@
-package Functions;public class RecursivePattern {
+package Functions;
+
+import java.util.Scanner;
+
+public class RecursivePattern {
+
+    public static int pattern(int number, int controlNumber, int controlNumber2) {
+        if (controlNumber > 0) {
+            if (number >= 0) {
+                System.out.print(number + " ");
+                return pattern(number - 5, controlNumber - 5, controlNumber2);
+            }
+        } else {
+            if (controlNumber2 >= number){
+                System.out.print(number + " ");
+                return pattern(number + 5, controlNumber, controlNumber2);
+            }
+        }
+        return number;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number, controlNumber, controlNumber2;
+        System.out.print("N sayısı: ");
+        number = input.nextInt();
+        controlNumber = number;
+        controlNumber2 = number;
+        pattern(number, controlNumber, controlNumber2);
+
+    }
 }
